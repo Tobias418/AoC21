31164e9cff59365f34088b60560407c117b07d9f
Tobias418 <29679503+Tobias418@users.noreply.github.com>
2021-12-01 23:54:39 +0100

    day 1

---
+++ src/savexmas/Day1.java
@@ -0,0 +1,29 @@
+package savexmas;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day1 extends AoCDay {
+
+    public static void main(String[] args) {
+        new Day1();
+    }
+
+    public long part1(List<String> input) {
+        long increaseCounter = 0;
+        List<Long> depths = input.stream().map(Long::parseLong).collect(Collectors.toList());
+        for (int i = 0; i < depths.size() - 1; i++) {
+            increaseCounter += depths.get(i) < depths.get(i + 1) ? 1 : 0;
+        }
+        return increaseCounter;
+    }
+
+    public long part2(List<String> input) {
+        long increaseCounter = 0;
+        List<Long> depths = input.stream().map(Long::parseLong).collect(Collectors.toList());
+        for (int i = 0; i < depths.size() - 3; i++) {
+            increaseCounter += depths.get(i) < depths.get(i + 3) ? 1 : 0; // credits to davids big brain
+        }
+        return increaseCounter;
+    }
+}
